09530ea604e6634ff90875531232d5e35fe1f9dc
Arsheen Pancar <pancar@usc.edu>
2025-04-25 07:46:38 +0000

    final commit

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,8 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool recurse(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, DailySchedule& sched, vector<size_t>& workerShifts, size_t day, size_t numAssigned);
+bool conditions(const AvailabilityMatrix& avail, DailySchedule& sched, vector<size_t>& workerShifts, size_t worker, size_t day, size_t maxShifts);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,9 +38,116 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    // needs to schedule d (needed) out of k (possible) workers per day 
+    size_t numWorkers = avail[0].size(); 
+    size_t numDays = avail.size(); 
+    // given their availability over n day period 
+    // exactly d workers required per day 
+    // no one can work more than m (maxshifts) over the n day period 
+    // function should schedule the nurses: 
+    // exactly d work on each of the n days and no one works more than m shifts 
+    
+    // avail matrix: n row by k column matrix of booleans 
+    // row represents on day of the schedule 
+    // column is specific worker's availability on that day 
 
+    // produce a schedule that is n by d matrix 
+    // row represents a day in the schedule & stores d (IDs of workers who are scheduled for that day)
+    // each entry is a value btwn 0 to k-1 
 
+    // initialize the sched matrix (the one we update as we go on):
+    sched = DailySchedule(numDays, vector<Worker_T>());
+    // vector of vectors indicating the d workers on each of n days 
+
+    // vector that tracks how many shifts each worker is already assigned (helps in checking conditions)
+    vector<size_t> workerShifts(numWorkers,0);
+
+    // start recursion now 
+    return recurse(avail, dailyNeed, maxShifts, sched, workerShifts, 0, 0);
+    // recursion starts at 0 
 
 
 }
 
+bool recurse(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, DailySchedule& sched, vector<size_t>& workerShifts, size_t day, size_t numAssigned)
+{
+    // must use recurisve backtracking approach 
+    // can use functions from algorithm such as find() 
+
+    // only use 3 loops: 
+    // 2 for set up and one in recursive search 
+
+    // backtracking: 
+    // recursive alg that generates all combos but skips recursion & moves on if any constraints are violated
+    // recurse over each place in the schedule 
+    // each recursive call fills in one of the n*d openings 
+    // make sure that the contrainsts (avaialble & maxshifts) are met
+
+    // general backtrack search approach: 
+    // 1. select an item & set it to one of its options 
+    // such as constraints are met 
+
+    // update variables: 
+    size_t numDays = avail.size(); 
+    size_t numWorkers = avail[0].size(); 
+
+    // terminating case: all days are filled 
+    if (day == numDays){
+        return true; 
+    }
+
+
+    // 2. recursively try to set next item 
+
+    // if the requirements for a day are met, move on to next day: 
+    if (numAssigned == dailyNeed){
+        return recurse(avail, dailyNeed, maxShifts, sched, workerShifts, day+1, 0);
+    }
+
+    // 3. if all items are assigned & constraints met, done
+    // return through recursion stack with solution 
+    // 4. if no viable value for an item exists, 
+    // backtrack to prev item & repeat from top 
+    // 5. if viable options for 1st item are exhausted, 
+    // no solution 
+
+    // 0 is false, any-non zero value is true (1 means avaliable) 
+
+    // helper function to take care of recursion 
+    // 2 loops for set up, 1 for recursion 
+    // loop through days & workers 
+
+    // using general approach from slides now: 
+    // now for loop does most of work: 
+    // goal: loop through all workers (for this day - we are only looking at one day at a time)
+    for (size_t cur = 0; cur < numWorkers; ++cur){
+        // call helper function to see if this worker can be used 
+        if (conditions(avail, sched, workerShifts, cur, day, maxShifts)){
+            // update variables (we are adding this worker in)
+            sched[day].push_back(cur);
+            workerShifts[cur]++;
+            // if worker is found, move on to next woker requirement slot & return true 
+            if (recurse(avail, dailyNeed, maxShifts, sched, workerShifts, day, numAssigned+1)){
+                return true; 
+            }
+            // if not found, then backtrack
+            // update variables 
+            sched[day].pop_back();
+            workerShifts[cur]--;  
+        }
+    // at end of this for loop, return false (backtracked too far and no solution)
+    }
+    return false; 
+}
+
+bool conditions(const AvailabilityMatrix& avail, DailySchedule& sched, vector<size_t>& workerShifts, size_t worker, size_t day, size_t maxShifts)
+{
+    // HELPER FUNCTION THAT CHECKS CONDITIONS
+    // 3 conditions: 
+    // check if worker is available & has not met max shifts & is not already assigned: 
+    if ((avail[day][worker]) && (workerShifts[worker] < maxShifts) && (find(sched[day].begin(), sched[day].end(), worker) == sched[day].end())){
+        return true; 
+    }
+    // need to also create a variable that tracks how many shifts each worker has worked <--
+    return false; 
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,16 +13,231 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void recursecombos(set<string>& allcombos, string& temp, const string& in, string& floating, size_t index, const set<string>& dict);
+set<string> check(const set<string>& allcombos, const set<string>& dict); 
 
+
+/**
+ * @brief Returns the list of all legal words in the provided dictionary
+ *        that meet the criteria provided in the `in` and `floating`
+ *        inputs.
+ * 
+ * @param [in] in - string of fixed, correct characters and `-`s to indicate
+ *                  the length of string and locations that must be filled in
+ * @param [in] floating  - Characters that must be used somewhere in the word
+ * @param [in] dict - Dictionary of strings of legal words
+ * @return std::set<std::string> - Set of all words that meet the criteria
+ */
 
 // Definition of primary wordle function
+// NOT PROVIDED: a list of gray letters (those not in the word)
+
+// approach: 
+// generate all possible strings of lower-case, alpha chracters 
+// contains the fixed characters (in their locations) & floating (any loc) & rest of word filled in 
+// then check if any of these are english language words --> this can be helper function 
+
+// requirements: 
+// use recursive approach to find all combinations of letters to form length -n word 
+// basically: have 1-2 loops to help set characters in any given location 
+// maybe 1-2 other loops to help with constraint checks 
+// DO NOT BRUTE FORCE THE SOLUTIONS 
+// use AT MOST 5 loops 
+// dont need to implement backtracking approach 
+// instead, just generate all possible strings then check if each word is in dict after it has all the letters 
+
+// hints: 
+// generating all combinations 
+// use recursion to build up a combinaion 1 "place" at a time 
+// each recursion is responsible for 1 "place/location" of the combination 
+// for that place, each recursion should try all the "options", recurse after each one 
+// upon return, undo and try the next option to get multiple solutions 
+// think abt "options" to try at each loc - what are limitations of letter usage 
+// then lastly just check if the combinations are valid english words 
+
+
+// idea: 
+// recursion w one of the checks being if there is already a char in the loc 
+// limitations to consider: 
+// floating letter doesnt mean it is the only case of that letter 
+// could create another func to handle the recursion? 
+// case to stop recursion = temp word is full (there are no '-')
+// get length variable 
+// temp variable 
 std::set<std::string> wordle(
     const std::string& in,
     const std::string& floating,
     const std::set<std::string>& dict)
 {
     // Add your code here
+	// size_t len = in.length(); 
+	// string temp = nullptr; 
 
+	// declare variables 
+	set<string> allcombos; 
+	// set to hold all the combos (used to temp hold the answer up until check)
+	string temp = in; 
+	// temp variable (imp for recursion) 
+	string updatedFloat = floating; 
+	// need this var to keep track 
+
+	recursecombos(allcombos, temp, in, updatedFloat, 0, dict); 
+	// first call --> will produce final once returned. indexing and temp added to assist in recursion here 
+
+	return check(allcombos, dict);
+	// return the result from check 
+	// check ensures that all the words that are returned are in the dictionary 
 }
 
 // Define any helper functions here
+// helper functions: one to recurse and one to check in the end 
+
+void recursecombos(set<string>& allcombos, string& temp, const string& in, string& floating, size_t index, const set<string>& dict){
+	// general recursion & combinations approach: 
+
+	// string alphabet = "abcdefghijklmnopqrstuvwxyz";
+	// data to iterate through all possible characters 
+
+	// string temp = nullptr; 
+	// need some array/vector/string to store partial answer as it is being built 
+
+	// need to track the locations that are empty 
+
+	// base case can stop when all n locations are set: 
+	if (index == in.size()){
+		// need to find out how many spots need to be filled: 
+		size_t emptyCount = count(temp.begin(), temp.end(), '-');
+		// to determine if it is full (index number will be the goal size at this point)
+		if (floating.empty() && dict.find(temp) != dict.end() && emptyCount == 0){
+			allcombos.insert(temp);
+		}
+		return; 
+		
+		// before adding to the allcombos, make sure to ensure that all the characters in floating are used 
+		// code to handle that: 
+		// variable to hold the word to iterate through 
+		// now loop through all characters in the floating  
+		// for (char c : cur){
+		// 	// get position of where the floasting is 
+		// 	size_t pos = copy.find(c);
+		// 	size_t ind = cur.find(c); 
+		// 	if (pos == string::npos){
+		// 		return; 
+		// 		// if it is not a position, that means this word is not valid since the floating letter is not in the word 
+		// 	}
+		// 	cur.erase(ind,1);
+		// 	copy.erase(pos,1);
+			// erase that floating letter from the word to update 
+
+		// // this part can just be replaced with an if statement if it is hanfled below: 
+		// if (dict.find(temp) == dict.end()){
+		// 	return; 
+		// }
+	}
+
+	// 	if (cur.empty()){
+	// 		allcombos.insert(temp);
+	// 		return; 
+	// 		// extra check for the duplicates since tests not passing 
+	// 	}
+
+	// 	// add to final variable and return so recursion can continue 
+	// 	return; 
+	// 	// string curr = temp; 
+	// 	// for (char c : floating){
+	// 	// 	size_t pos = curr.find(c);
+	// 	// 	if (pos == string::npos){
+	// 	// 		return; 
+	// 	// 	}
+	// 	// 	curr.erase(pos,1);
+	// 	// }
+	// 	// allcombos.insert(temp);
+	// 	// return; 
+	// }
+
+	// then recurse to handle all remaining locations (i+1 to n)
+	// upon return, try another option value & recurse again 
+	// recursive case returns after trying all options 
+
+	// else{
+		// if not base case 
+		// first check to make sure that the spot is open 
+		// if spot is not open, recurse so you can move on to the next pos 
+		if (temp[index] != '-'){
+			recursecombos(allcombos, temp, in, floating, index + 1, dict);
+		}
+		else{
+			// recount empty spots 
+			size_t emptyCount = count(temp.begin() + index, temp.end(), '-');
+		// make sure solution is even possible by checking to make sure there are not more floating chars
+		if (floating.size() > emptyCount){
+			return; 
+		}
+
+		// PROBLEM IN MY CODE: DOES NOT ACCOUNT FOR REPEATING IN FLOATING 
+		// solution: keep a set to see if it is used 
+		
+		// loop through floating first is solution 
+		for (size_t i = 0; i < floating.size(); i++){
+			string next = temp; 
+			next[index] = floating[i]; 
+
+			string updated = floating; 
+			updated.erase(i,1);
+			recursecombos(allcombos, next, in, updated, index+1, dict);
+		}
+		
+			// this means that the spot is empty 
+			// to keep track of what is empty: 
+			// int empty = count(temp.begin() + index, temp.end(), '-');
+			// if (empty < static_cast<int>(floating.size())){
+			// 	return; 
+			// }
+			// function iteratively (loop) try each optiion in set of values by setting loc i to current option 
+			// iterate through alphabet - for loop of all chars a-z 
+		if (floating.length() < emptyCount){
+			for (char letter = 'a'; letter <= 'z'; letter++){
+				// each recursive call is responsible for one of the n "places" (ex: location i)
+				// if you go through this loop, it will handle all the floating characters 
+				// only issue is to update the floating characters 
+				// same approach as base case 
+
+				// first set the letter 
+				string next = temp; 
+				next[index] = letter; 
+				recursecombos(allcombos, next, in, floating, index+1, dict);
+
+				// int empty = count(temp.begin() + index + 1, temp.end(), '-');
+				// if (empty < static_cast<int>(floating.size())){
+				// 	if (check){
+				// 		floating.insert(floating.begin() + ind, letter);
+				// 	}
+				// 	continue; 
+				// }
+				
+				// call recursion on the next i pos at this point! 
+				// but all options of the current i have not executed yet ...
+				//recursecombos(allcombos, temp, in, floating, index+1, dict);
+				// recursive call with new floating set (inserted)
+
+				// if (check){
+				// 	floating.insert(floating.begin() + ind, letter); 
+				// }
+			}
+		}
+	}
+}
+
+set<string> check(const set<string>& allcombos, const set<string>& dict){
+	set<string> result; 
+	// variable to hold what will be returned 
+	for (const string& word : allcombos){
+		// loop through all words in the allcombos (this type of iteration is easier to handle)
+		if (dict.find(word) != dict.end()){
+			// if word is in the dictionary, add it into the result 
+			result.insert(word);
+		}
+	}
+	return result; 
+	// return the result (this is the final answer)
+}
